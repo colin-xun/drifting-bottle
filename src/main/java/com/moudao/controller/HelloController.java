d6c930d5e300da3b7b222947d249f8e7ce28714e
tom <tom123@163.com>
2018-03-27 00:28:34 +0800

    第一次初始推送

---
+++ src/main/java/com/moudao/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.moudao.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * author: MrWang
+ * date: 2018/3/26 23:56
+ */
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/index", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public String hello() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/moudao/service/impl/RealRealm.java
@@ -0,0 +1,24 @@
+package com.moudao.service.impl;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * author: MrWang
+ * date: 2018/3/26 22:41
+ */
+public class RealRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
