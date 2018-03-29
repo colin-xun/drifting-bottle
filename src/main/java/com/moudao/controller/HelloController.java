0869674eaab88a5664e325d7f2c92938bb354da8
flying81621 <flying81621@163.com>
2018-03-29 23:03:37 +0800

    测试hellocontroller

--- src/main/java/com/moudao/controller/HelloController.java
+++ src/main/java/com/moudao/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.moudao.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class HelloController {
+    private static final Log log = LogFactory.getLog(HelloController.class);
+
     @RequestMapping(value = "/index", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public String hello() {
         return "index";
